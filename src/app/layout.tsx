8dd8b2f1b0a784e311bc49e9c7f51036a1860f71
CamiloLima123 <celulardocamilo@gmail.com>
2026-02-16 22:00:05 -0300

    layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Refrigelar",
+  description: "Sistema Refrigelar",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
